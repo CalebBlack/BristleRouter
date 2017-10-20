02d3d572b36001cc460ac9087f0f3750115e8251
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-10-20 14:44:05 -0600

    create repo' git

---
+++ bristlerouter.js
@@ -0,0 +1,78 @@
+const Bristle = require('bristle');
+function createRouter(map){
+  var router = new Bristle('router');
+  var routes = Object.assign({},map);
+  new BristleRouter(router,routes);
+  return router;
+}
+class BristleRouter {
+  constructor(bristle,routes){
+    this.checkURL = this.checkURL.bind(this);
+    this.setURL = this.setURL.bind(this);
+    this.getMatchingRoute = this.getMatchingRoute.bind(this);
+    this.URL = document.URL;
+    this.bristle = bristle;
+    this.routes = routes;
+	  window.addEventListener("hashchange",(event)=>{
+      this.checkURL();
+    }
+    this.render();
+	});
+  }
+  render(){
+    this.bristle.children = [];
+    var element = this.bristle.element;
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+    var currentPath = this.getMatchingRoute(this.URL);
+    if (currentPath && this.routes.hasOwnProperty('currentPath')){
+      var child = this.routes[currentPath];
+      if (child instanceof HTMLElement) {
+        this.bristle.render(child);
+      } else if (child instanceof Bristle) {
+        child.appendTo(this.bristle);
+      }
+    }
+  }
+  getMatchingRoute(pathIn){
+    var inputPath = this.getParts(pathIn);
+    var output = null;
+    Object.keys(this.routes).forEach(checkPath=>{
+      var checkParts = this.getParts(checkPath);
+      if (this.doRouteArraysMatch(checkParts,this.url)){
+        output = checkPath;
+      }
+    });
+    return output;
+  }
+  doRouteArraysMatch(routepath,currentpath){
+    for (var i = 0; i < arr1.length; i++) {
+      if (routePath[i] !== '*' && routePath[i] !== currentpath[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  getPath(parts){
+    return '/'+parts.join('/');
+  }
+  getParts(path){
+    if (!typeof path === 'string' || path.length <= 0) {
+      throw this.error('Invalid Path!')
+    }
+    return path.split('/').filter((path)=>{return path}););
+  }
+  checkURL(){
+    if (this.lastURL !== document.URL) {
+      this.setURL(document.URL);
+    }
+  }
+  setURL(URL) {
+    this.URL = URL;
+    this.render();
+  }
+  error(str){
+    return new Error('BristleRouter Error: '+str);
+  }
+}
